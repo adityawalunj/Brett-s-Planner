bf3921841d57f553e052925892c9aab5e5f249a5
KaranPurohit <45612915+KaranPurohit@users.noreply.github.com>
2019-01-21 14:36:09 +1000

    Add files via upload

---
+++ ict/projectict/calender_process.php
@@ -0,0 +1,42 @@
+<?php
+//must appear BEFORE the <html> tag
+include_once('include/config.php');	
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> process </title>
+</head>
+
+<?php
+if(isset($_POST['select_date'], $_POST['fromtime'], $_POST['totime'])) {
+	//make the database connection
+	$conn  = db_connect();
+	$date1 = $conn -> real_escape_string($_POST['select_date']);
+	$fromtime = $conn -> real_escape_string($_POST['fromtime']);
+	$totime = $conn -> real_escape_string($_POST['totime']);
+	 
+	
+	//create an insert query 	
+	$sql = "insert into personal_appointment(calendar, from_time, to_time) 
+			VALUES ('$date1', '$fromtime', '$totime')";
+	//execute the query
+	if($conn -> query($sql))
+	{
+
+		echo "<h1></h1>";
+		echo "<p>Hi inserted sucessfully <b>$name</b></p>";
+
+	}
+	$conn -> close();		
+}
+else {
+	die("Input error");
+}
+?>    
+
+</div>
+</body>
+</html>
+
---
+++ ict/projectict/include/config.php
@@ -0,0 +1,19 @@
+<?php
+//define constants for connection info
+define("MYSQLUSER","ictatjcu_portal");
+define("MYSQLPASS","123zxc");
+define("HOSTNAME","localhost");
+define("MYSQLDB","ictatjcu_portal");
+
+//make connection to database
+function db_connect()
+{
+	$conn = @new mysqli(HOSTNAME, MYSQLUSER, MYSQLPASS, MYSQLDB);
+	if($conn -> connect_error) {
+		die('Connect Error: ' . $conn -> connect_error);
+	}
+	return $conn;
+} 
+?>
+
+
